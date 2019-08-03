290de21599692c9487b351767c5782383eb873bf
dvvadvva <dvva@inbox.ru>
2019-08-03 22:20:10 +0300

    1

--- src/component/ticket/oneTicket.js
+++ src/component/ticket/oneTicket.js
@@ -7,25 +7,28 @@ class Ticket extends Component {
         return (
             <div className={s.allticket}>
                 <div className={s.container}>
-                    <div className={s.p1}>{this.props.cost} {this.props.curency.curValuta}</div>
-                    <div className={s.p2}></div>
-                    <div className={s.p3}>{this.props.ticket.carrier}</div>
+                    <div className={s.cost}>{this.props.cost} {this.props.curency.curValuta}</div>
+                    <div></div>
+                    <div>
+                        <img src={`https://pics.avs.io/al_square/28/28/${this.props.ticket.carrier}@2x.png`} alt={this.props.ticket.carrier} />
+                    </div>
 
-                    <div className={s.p4}>{this.props.segmentTo.origin} - {this.props.segmentTo.destination}</div>
-                    <div className={s.p5}>в пути</div>
-                    <div className={s.p6}>{this.props.ticket.CountStopsTo} пересадки</div>
+                    <div className={s.bledno}>{this.props.segmentTo.origin} - {this.props.segmentTo.destination}</div>
+                    <div className={s.bledno}>в пути</div>
+                    <div className={s.bledno}>{this.props.ticket.CountStopsTo} пересадки</div>
 
-                    <div className={s.p7}>{this.props.segmentTo.timeDeparture}-{this.props.segmentTo.timeArrival}</div>
-                    <div className={s.p8}>{this.props.segmentTo.durationHH}: {this.props.segmentTo.durationMM}</div>
-                    <div className={s.p9}>{this.props.ticket.listPeresadTo}</div>
+                    <div>{this.props.segmentTo.timeDeparture}-{this.props.segmentTo.timeArrival}</div>
+                    <div>{this.props.segmentTo.durationHH}: {this.props.segmentTo.durationMM}</div>
+                    <div>{this.props.ticket.listPeresadTo}</div>
 
-                    <div className={s.p10}><div className={s.p4}>{this.props.segmentBack.destination} - {this.props.segmentBack.origin}</div></div>
-                    <div className={s.p11}>в пути</div>
-                    <div className={s.p12}>{this.props.ticket.CountStopsBack} пересадки</div>
+                    <div className={s.bledno}>{this.props.segmentBack.destination} - {this.props.segmentBack.origin}</div>
+                    <div className={s.bledno}>в пути</div>
+                    <div className={s.bledno}>{this.props.ticket.CountStopsBack} пересадки</div>
 
-                    <div className={s.p13}>{this.props.segmentBack.timeDeparture}-{this.props.segmentBack.timeArrival}</div>
-                    <div className={s.p14}>{this.props.segmentBack.durationHH}: {this.props.segmentBack.durationMM}</div>
-                    <div className={s.p15}>{this.props.ticket.listPeresadBack}</div>
+                    <div>{this.props.segmentBack.timeDeparture}-{this.props.segmentBack.timeArrival}</div>
+                    <div>{this.props.segmentBack.durationHH}: {this.props.segmentBack.durationMM}</div>
+                    <div>{this.props.ticket.listPeresadBack}</div>
+        
                 </div>
             </div>
         )
